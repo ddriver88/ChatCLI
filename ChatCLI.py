adef0bac8d5b11a62981f3f224c923aa221db672
Daniel Driver <daniel.driver88@gmail.com>
2023-04-23 21:24:20 -0500

    Increased tokens for better responses
    It will take a little longer to send the response, but it's much more thorough

--- ChatCLI.py
+++ ChatCLI.py
@@ -12,7 +12,7 @@ def generate_response(prompt):
     response = openai.Completion.create(
         engine="text-davinci-002",
         prompt=prompt,
-        max_tokens=1024,
+        max_tokens=4000,
         n=1,
         stop=None,
         temperature=0.5,
